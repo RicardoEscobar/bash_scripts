7202acec2e8cb5253dda52f021e3d16a6f943836
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2023-04-17 18:08:58 -0600

    Working version of bash_scripts/compress-folder.sh

--- compress-folder.sh
+++ compress-folder.sh
@@ -10,13 +10,10 @@
 # The script will also create a log file inside the given folder.
 # The script will also create a file called "compressed.txt" inside the given folder and put the names of the compressed files there.
 # The script will also create a file called "not_compressed.txt" inside the given folder and put the names of the files that were not compressed there.
-# The script will also create a file called "not_found.txt" inside the given folder and put the names of the files that were not found there.
-# The script will also create a file called "not_video.txt" inside the given folder and put the names of the files that were not video files there.
 #: Options : None
 
 # Get the folder path from the argument. Considering folder paths can have spaces, we need to use "$1" instead of $1.
 folder_path="$1"
-echo "Folder path: $folder_path"
 
 # Create the compressed folder.
 mkdir -p "$folder_path/compressed"
@@ -30,18 +27,12 @@ touch "$folder_path/compressed.txt"
 # Create the not_compressed.txt file.
 touch "$folder_path/not_compressed.txt"
 
-# Create the not_found.txt file.
-touch "$folder_path/not_found.txt"
-
-# Create the not_video.txt file.
-touch "$folder_path/not_video.txt"
-
 # Get the list of MKV files in the given folder.
 mkv_files=$(find "$folder_path" -type f -name "*.mkv")
-echo "mkv_files: $mkv_files"
 
 # Loop through the MKV files.
-for mkv_file in "$mkv_files"; do
+IFS=$'\n' # Set the internal field separator to newline to handle paths with spaces correctly.
+for mkv_file in $mkv_files; do
     echo "Processing $mkv_file"
     # Get the file name.
     file_name=$(basename "$mkv_file")
@@ -53,7 +44,13 @@ for mkv_file in "$mkv_files"; do
     file_path=$(dirname "$mkv_file")
 
     # Compress the MKV file as MP4.
-    ffmpeg -i "$mkv_file" -c:v libx264 -crf 23 -preset slow -c:a copy "$folder_path/compressed/$file_name_no_ext.mp4" >> "$folder_path/compressed.log" 2>&1
+    nohup ffmpeg -hide_banner -loglevel error -i "${mkv_file}" -c:v libx264 -crf 23 -preset slow -c:a copy "${folder_path}compressed/$file_name_no_ext.mp4" >> "${folder_path}compressed.log" 2>&1 &
+
+    # Echo the command.
+    # echo "nohup ffmpeg -hide_banner -loglevel error -i "${mkv_file}" -c:v libx264 -crf 23 -preset slow -c:a copy "${folder_path}compressed/$file_name_no_ext.mp4" >> "${folder_path}compressed.log" 2>&1 &"
+
+    # Wait for 1 second.
+    sleep 1
 
     # Check if the file was compressed.
     if [ -f "$folder_path/compressed/$file_name_no_ext.mp4" ]; then
